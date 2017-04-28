cf24985397a2e5181fac629dfdd6152a42438dc7
Matthew Dever <Matthew@matthewdever.ca>
2017-04-27 23:54:17 -0400

    fixed site to bootstrap 4

--- components/tasksList.js
+++ components/tasksList.js
@@ -3,7 +3,7 @@ angular.module('mattTodo').component('tasksList', {
     controller: function TaskListController($scope,$http){
         console.log("Loading tasks...");
         //Here is code to read and write from localstorage
-        
+
         $scope.saved = localStorage.getItem('tasks');
         $scope.tasks = (localStorage.getItem('tasks')!==null) ? JSON.parse($scope.saved) : [
           {
@@ -11,6 +11,7 @@ angular.module('mattTodo').component('tasksList', {
             "title":"Build this amazing Todo List",
             "description":"This is a hugely midterm project, so you better get this right",
             "dueDate":"2017-03-10T05:00:00.000Z",
+            "category":"default",
             "isCompleted":false
           },
           {
@@ -18,11 +19,12 @@ angular.module('mattTodo').component('tasksList', {
             "title":"Let us Edit the marvy Todos",
             "description":"We need to be able to edit these things",
             "dueDate":"2017-03-09T05:00:00.000Z",
+            "category":"testing",
             "isCompleted":false
           }];
-        
+
         localStorage.setItem('tasks', JSON.stringify($scope.tasks));
-        
+
         //count remaining tasks
         $scope.remaining = function() {
             var count = 0;
@@ -31,10 +33,10 @@ angular.module('mattTodo').component('tasksList', {
             });
             return count;
         };
-        
+
         //add Item to LocalStorage
         $scope.addTodo = function() {
-           console.log("adding..."); 
+           console.log("adding...");
             $scope.tasks.push({
                 id: cuid(),
                 title: $scope.todoTitle,
@@ -45,10 +47,11 @@ angular.module('mattTodo').component('tasksList', {
             $scope.todoTitle = ''; //clear the input after adding
             $scope.todoDate = ''; //clear the input after adding
             $scope.todoDescription = ''; //clear the input after adding
+            $scope.todoCategory = ''; //clear the input after adding
 
             localStorage.setItem('tasks', JSON.stringify($scope.tasks));
 	    };
-        
+
         // Delete Completed Tasks
         $scope.archive = function() {
 		var oldTasks = $scope.tasks;
@@ -59,7 +62,7 @@ angular.module('mattTodo').component('tasksList', {
 		});
 		localStorage.setItem('tasks', JSON.stringify($scope.tasks));
 	   };
-        
+
         //Delete Sekected Task
         $scope.deleteTask = function(selectedTask) {
             console.log("Deleting task: " + selectedTask.title + " ID: " + selectedTask.id);
@@ -76,20 +79,20 @@ angular.module('mattTodo').component('tasksList', {
             console.log("Tasks After filter:" +$scope.tasks);
 		localStorage.setItem('tasks', JSON.stringify($scope.tasks));
 	   };
-        
+
         $scope.convertToDate = function (stringDate){
             var dateOut = new Date(stringDate);
             dateOut.setDate(dateOut.getDate() + 1);
             return dateOut;
         };
-        
+
         //Update isCompleted Checkbox to localStorage
         $scope.updateChecked = function(){
             console.log('Updated Checked Status: '  );
             var oldTasks = $scope.tasks;
             $scope.tasks = [];
             angular.forEach(oldTasks, function(task){
-            //write updated info to localstorage    
+            //write updated info to localstorage
                 $scope.tasks.push(task);
             });
             localStorage.setItem('tasks', JSON.stringify($scope.tasks));
@@ -101,14 +104,14 @@ angular.module('mattTodo').component('tasksList', {
             $scope.selected = angular.copy(task);
             console.log("selected:" + $scope.selected);
         };
-        
+
         $scope.saveTask = function(task){
             console.log("saving task:" + task);
             console.log('Updated Checked Status: '  );
             var oldTasks = $scope.tasks;
             $scope.tasks = [];
             angular.forEach(oldTasks, function(task){
-            //write updated info to localstorage    
+            //write updated info to localstorage
                 $scope.tasks.push(task);
             });
             localStorage.setItem('tasks', JSON.stringify($scope.tasks));
@@ -117,17 +120,17 @@ angular.module('mattTodo').component('tasksList', {
         };
         $scope.clearTask = function(row){
             console.log("clearing task row:" + row);
-            row.$rollbackViewValue() 
-            $scope.selected = {}; 
+            row.$rollbackViewValue()
+            $scope.selected = {};
             console.log("selected:" + $scope.selected);
         };
-        
-        $scope.selected = {}; 
-        $scope.getTemplate = function (task) {  
-            if (task.id === $scope.selected.id){  
-                return 'templates/tasks-list-editrow.template.html';  
-            }  
-            else return 'templates/tasks-list-displayrow.template.html';  
+
+        $scope.selected = {};
+        $scope.getTemplate = function (task) {
+            if (task.id === $scope.selected.id){
+                return 'templates/tasks-list-editrow.template.html';
+            }
+            else return 'templates/tasks-list-displayrow.template.html';
         };
 //        console.log ("Task0: " & $scope.tasks);
         //This is the old code that reads from json file
